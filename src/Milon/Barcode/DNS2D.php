a44018373cbf83d6469bde7710b2061f323e79ef
StylFlex <antoinev@seatfrog.com>
2016-07-29 14:02:28 +1000

    UPDATED: Add option to set Background Color and Foreground Color

--- src/Milon/Barcode/DNS2D.php
+++ src/Milon/Barcode/DNS2D.php
@@ -173,31 +173,29 @@ class DNS2D {
      * <li>$arrcode['bcode'][$r][$c] value of the cell is $r row and $c column (0 = transparent, 1 = black)</li></ul>
      * @param $code (string) code to print
      * @param $type (string) type of barcode: <ul><li>DATAMATRIX : Datamatrix (ISO/IEC 16022)</li><li>PDF417 : PDF417 (ISO/IEC 15438:2006)</li><li>PDF417,a,e,t,s,f,o0,o1,o2,o3,o4,o5,o6 : PDF417 with parameters: a = aspect ratio (width/height); e = error correction level (0-8); t = total number of macro segments; s = macro segment index (0-99998); f = file ID; o0 = File Name (text); o1 = Segment Count (numeric); o2 = Time Stamp (numeric); o3 = Sender (text); o4 = Addressee (text); o5 = File Size (numeric); o6 = Checksum (numeric). NOTES: Parameters t, s and f are required for a Macro Control Block, all other parametrs are optional. To use a comma character ',' on text options, replace it with the character 255: "\xff".</li><li>QRCODE : QRcode Low error correction</li><li>QRCODE,L : QRcode Low error correction</li><li>QRCODE,M : QRcode Medium error correction</li><li>QRCODE,Q : QRcode Better error correction</li><li>QRCODE,H : QR-CODE Best error correction</li><li>RAW: raw mode - comma-separad list of array rows</li><li>RAW2: raw mode - array rows are surrounded by square parenthesis.</li><li>TEST : Test matrix</li></ul>
-     * @param $w (int) Width of a single rectangle element in pixels.
-     * @param $h (int) Height of a single rectangle element in pixels.
+     * @param $size (int) Width and Height of a single rectangle element in pixels.
      * @param $color (array) RGB (0-255) foreground color for bar elements (background is transparent).
+     * @param $bgColor (array) RGB (0-255) background color for background elements.
      * @return path or false in case of error.
      * @public
      */
-    public function getBarcodePNG($code, $type, $w = 3, $h = 3, $color = array(0, 0, 0)) {
-        if (!$this->store_path) {
-            $this->setStorPath(\Config::get("barcode.store_path"));
-        }
+    public function getBarcodePNG($code, $type, $size = 3, $color = [0, 0, 0],$bgColor = [255,255,255]) {
+
         //set barcode code and type
         $this->setBarcode($code, $type);
         // calculate image size
-        $width = ($this->barcode_array['num_cols'] * $w);
-        $height = ($this->barcode_array['num_rows'] * $h);
+        $width = ($this->barcode_array['num_cols'] * $size);
+        $height = ($this->barcode_array['num_rows'] * $size);
         if (function_exists('imagecreate')) {
             // GD library
             $imagick = false;
             $png = imagecreate($width, $height);
-            $bgcol = imagecolorallocate($png, 255, 255, 255);
-            imagecolortransparent($png, $bgcol);
+            $bgcol = imagecolorallocate($png, $bgColor[0], $bgColor[1], $bgColor[2]);
+            imagefill($png, 0, 0, $bgcol);
             $fgcol = imagecolorallocate($png, $color[0], $color[1], $color[2]);
         } elseif (extension_loaded('imagick')) {
             $imagick = true;
-            $bgcol = new \imagickpixel('rgb(255,255,255');
+            $bgcol = new \imagickpixel('rgb(' . $bgColor[0] . ',' . $bgColor[1] . ',' . $bgColor[2] . ')');
             $fgcol = new \imagickpixel('rgb(' . $color[0] . ',' . $color[1] . ',' . $color[2] . ')');
             $png = new \Imagick();
             $png->newImage($width, $height, 'none', 'png');
@@ -208,6 +206,8 @@ class DNS2D {
         }
         // print barcode elements
         $y = 0;
+        $w = $size;
+        $h = $size;
         // for each row
         for ($r = 0; $r < $this->barcode_array['num_rows']; ++$r) {
             $x = 0;
@@ -237,7 +237,6 @@ class DNS2D {
         }
         $image = ob_get_clean();
         $image = base64_encode($image);
-        //$image = 'data:image/png;base64,' . base64_encode($image);
         return $image;
     }
 
